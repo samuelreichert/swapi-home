d36d294b7b0627a754e4a5e9cdadc561e125310a
Samuel Reichert <samuel.reichert@helloprint.com>
2021-12-21 00:31:51 +0100

    Add react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,31 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { Routes, Route } from 'react-router-dom';
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
-
+import { ThemeProvider } from 'styled-components';
 import Home from './pages/Home';
 import Characters from './pages/Characters';
 import About from './pages/About';
 
+const queryClient = new QueryClient();
+
 const theme = {
   colors: {
-    black: '#000',
-    white: '#fff',
-    gray10: '#101416',
-    gray9: '#212529',
-    gray8: '#343a40',
-    gray7: '#495057',
-    gray6: '#868e96',
-    gray5: '#adb5bd',
-    gray4: '#ced4da',
-    gray3: '#dee2e6',
-    gray2: '#e9ecef',
-    gray1: '#f8f9fa'
+    black: '#101416',
+    grayDark: '#343a40',
+    grayMedium: '#495057',
+    grayLight: '#dee2e6',
+    white: '#f8f9fa'
   }
 };
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    color: #fff;
-  }
-`;
-
 const App = () => (
   <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='characters' element={<Characters />} />
-      <Route path='about' element={<About />} />
-    </Routes>
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='characters' element={<Characters />} />
+        <Route path='about' element={<About />} />
+      </Routes>
+    </QueryClientProvider>
   </ThemeProvider>
 );
 
